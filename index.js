44c9f192e6bbdd8a9ca291854a13f8d76906949f
vishnugopy <vishnugoby0010@gmail.com>
2021-03-02 16:40:09 +0100

    :tada: Projet est fini

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ let buttons = document.querySelectorAll("button");
 let gamestatus = document.querySelector("#game_status");
 
 let player1 = "X";
-
 let player2 = "O";
 //console.log(buttons);
 
-
 let Gamecheck;
 let count = 1;
-
 
 buttons.forEach(element => {
     element.addEventListener("click", () => {
@@ -20,7 +17,7 @@ buttons.forEach(element => {
                 element.textContent = player1;
                 element.style.color = "red";
                 gamestatus.textContent = "Joueur O c'est votre tour.";
-                gamestatus.style.backgroundColor = "#34cfeb";
+                gamestatus.style.backgroundColor = "#00d8ff";
                 count++;
             } else {
                 element.style.color = "#34b1eb";
@@ -30,40 +27,42 @@ buttons.forEach(element => {
                 count--;
             }
         }
-
-
-
-
-
+        
         //tetsting horizontally
         if (buttons[0].textContent == buttons[1].textContent && buttons[1].textContent == buttons[2].textContent) {
             Gamecheck = buttons[2].textContent + " win";
-            console.log(buttons[2].textContent + " win");
+            
             gameover();
         } else if (buttons[3].textContent == buttons[4].textContent && buttons[4].textContent == buttons[5].textContent) {
             Gamecheck = buttons[5].textContent + " win";
             gameover();
         } else if (buttons[6].textContent == buttons[7].textContent && buttons[7].textContent == buttons[8].textContent) {
             Gamecheck = buttons[8].textContent + " win";
+            
             gameover();
         }
         //tetsting verticlay
         else if (buttons[0].textContent == buttons[3].textContent && buttons[3].textContent == buttons[6].textContent) {
             Gamecheck = buttons[6].textContent + " win";
+            
             gameover();
         } else if (buttons[1].textContent == buttons[4].textContent && buttons[4].textContent == buttons[7].textContent) {
             Gamecheck = buttons[7].textContent + " win";
+            
             gameover();
         } else if (buttons[2].textContent == buttons[5].textContent && buttons[5].textContent == buttons[8].textContent) {
             Gamecheck = buttons[8].textContent + " win";
+            
             gameover();
         }
         //testing jagunbal
         else if (buttons[0].textContent == buttons[4].textContent && buttons[4].textContent == buttons[8].textContent) {
             Gamecheck = buttons[8].textContent + " win";
+            
             gameover();
         } else if (buttons[2].textContent == buttons[4].textContent && buttons[4].textContent == buttons[6].textContent) {
             Gamecheck = buttons[6].textContent + " win";
+            
             gameover();
         }
     })
@@ -72,11 +71,12 @@ buttons.forEach(element => {
 function gameover() {
     setTimeout(() => {
         if (Gamecheck == "X win") {
-            alert("x win")
+            alert("Player X Won");
             window.location.href = "index.html";
         } else if (Gamecheck == "O win") {
-            alert("o win")
+            alert("Player O Won")
             window.location.href = "index.html";
         }  
-    }, 200);
+    }, 100);
 }
+
